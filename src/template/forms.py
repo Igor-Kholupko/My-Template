900f353fd506ed57867f82f7fc36ca51108e4ee6
Igor Kholupko <mholupko0@gmail.com>
2018-11-05 02:54:20 +0300

    Обновление приложения "Шаблоны"
    Создание полноценного приложения "Шаблоны":
        Создание модели шаблона с полями:
            - название;
            - владелец (может быть Null - анонимный пользователь);
            - email (для анонимных пользователей);
            - файл;
            - флаг 'опубликован';
            - описание.
    
    Создание класса-представления, формы, а так же html-шаблонов для
    загрузки\публикации шаблона:
        Создание полноценного класса-представления для загрузки\публикации:
            - класс определяет, какой пользователь отправляет запрос и на
              основе этого выполняет соответствующие операции.
    
        Создание форм для представлений:
            - форма для класса-представления загрузки\публикации. Поля:
                  - название;
                  - файл;
                  - email (для анонимных пользователей);
                  - флаг 'опубликован' (для авторизированных пользователей);
                  - описание.
            - форма для стандартных классов-представления администраторского
              сайта (создания и редактирования).
              Поля при создании:
                  - название;
                  - файл;
                  - владелец;
                  - email (если владельцем задан анонимный пользователь).
              Поля редактирования:
                  - название;
                  - владелец (только просмотр);
                  - email (только просмотр);
                  - флаг 'опубликован';
                  - описание;
                  - файл.
    
        Редактирование старых и создание новых шаблонов:
            - шаблон регистрации представлял собой шаблон для создания
              объекта, поэтому он был вынесен, как базовый шаблон создания;
            - наследование шаблона регистрации и загрузки\публикации от
              базового шаблона создания;
            - создание ссылки перехода на страницу администрации для
              уполномоченных пользователей;
            - прочие мелкие изменения (применение стиля, например).

---
+++ src/template/forms.py
@@ -0,0 +1,73 @@
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from .models import Template
+
+
+class UserModelChoiceField(forms.ModelChoiceField):
+    def __new__(cls, *args, **kwargs):
+        instance = forms.ModelChoiceField(*args, **kwargs)
+        instance.empty_label = _("Anonymous user")
+        return instance
+
+
+class TemplateAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        instance = getattr(self, 'instance', None)
+        if instance and instance.id:
+            try:
+                self.fields['user'].widget.attrs['readonly'] = True
+            except KeyError:
+                pass
+
+    def _clean_fields(self):
+        super()._clean_fields()
+        email_error = self._errors.get('email')
+        if email_error is not None and (email_error.data.__len__() == 1 and email_error.data[0].code == 'required'):
+            if self.cleaned_data.get('user') is not None:
+                self._errors.pop('email')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data.get('user') is not None:
+            cleaned_data.update({'email': cleaned_data.get('user').email})
+        return cleaned_data
+
+    def clean_user(self):
+        instance = getattr(self, 'instance', None)
+        if instance and instance.id:
+            return instance.user
+        else:
+            return self.cleaned_data['user']
+
+    class Meta:
+        model = Template
+        fields = '__all__'
+        field_classes = {
+            'user': UserModelChoiceField
+        }
+
+
+class TemplateCreateForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if 'name' in self.fields:
+            self.fields['name'].widget.attrs.update({'class': 'vTextField'})
+        if 'email' in self.fields:
+            self.fields['email'].widget.attrs.update({'class': 'vTextField'})
+
+    def _clean_fields(self):
+        super()._clean_fields()
+        email_error = self._errors.get('email')
+        if email_error is not None and (email_error.data.__len__() == 1 and email_error.data[0].code == 'required'):
+            if self.cleaned_data.get('user') is not None:
+                self._errors.pop('email')
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data.get('user') is not None:
+            cleaned_data.update({'email': cleaned_data.get('user').email})
+
+    class Meta:
+        model = Template
+        fields = ('name', 'email', 'file', 'is_shared', 'description', 'user')
